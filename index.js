286c7b49518ca037f5479e67e6e197bc30918b4a
Filip <fwan24@gmail.com>
2019-09-12 02:49:09 +0200

    Add development db connection string

--- index.js
+++ index.js
@@ -16,12 +16,9 @@ if (!config.get('jwtPrivateKey')) {
 
 app.set('view-engine','ejs');
 
-mongoose.connect(config.get('db'))
-
-mongoose.connect('mongodb+srv://Wojtek:coderscamp_WGF@tdacluster-yvt8y.azure.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(config.get('db'), {useNewUrlParser: true, useUnifiedTopology: true})
   .then(() => console.log('Connected to MongoDB...'))
   .catch(err => console.error('Could not connect to MongoDB...'));
-
 
 app.use(express.json());
 app.use('/api/auth', auth);
