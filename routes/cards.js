4ec8c36af03366d0c4ec30a5f9e9a937d8579567
Filip <fwan24@gmail.com>
2019-09-12 16:39:23 +0200

    Small refactoring + comments

--- routes/cards.js
+++ routes/cards.js
@@ -4,47 +4,51 @@ const express = require('express');
 const router = express.Router();
 
 
-  router.get('/', auth, async (req, res) => {
-    const card = await Card.find();
-    res.send(card);
-  });
+router.get('/', auth, async (req, res) => {
+  const card = await Card.find();
+  res.send(card);
+});
 
-  router.get('/:id', auth, async (req, res) => {
-    const card = await Card.findById(req.card._id);
-    res.send(card);
-  });
- 
-  // router.post('/', auth,  async (req, res) => {
-  //   const { error } = validate(req.body); 
-  //   if (error) return res.status(400).send(error.details[0].message);
-  
-  //   card = new Card(_.pick(req.body, ['title', 'description']));
-  //   await card.save();
-  
-  //   res.send(card);
-  // });
+router.get('/:id', auth, async (req, res) => {
+  const card = await Card.findById(req.card._id);
+  res.send(card);
+});
 
-  
+// router.post('/', auth,  async (req, res) => {
+//   const { error } = validate(req.body); 
+//   if (error) return res.status(400).send(error.details[0].message);
+
+//   card = new Card(_.pick(req.body, ['title', 'description']));
+//   await card.save();
+
+//   res.send(card);
+// });
+
 router.put('/:id', auth,  async (req, res) => {
-    const { error } = validate(req.body); 
-    if (error) return res.status(400).send(error.details[0].message);
-  
-    const card = await Card.findByIdAndUpdate(req.params.id, { title: req.body.title, description: req.body.description }, {
-      new: true
-    });
-  
-    if (!card) return res.status(404).send('The card with the given ID was not found.');
-    
-    res.send(card);
+  const { error } = validate(req.body); 
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const card = await Card.findByIdAndUpdate(req.params.id, { title: req.body.title, description: req.body.description }, {
+    new: true
   });
 
+  if (!card) return res.status(404).send('The card with the given ID was not found.');
   
+  res.send(card);
+});
+
+// Removes card from it's parent list and deletes card
 router.delete('/:id', auth, async (req, res) => {
-    const card = await Card.findByIdAndRemove(req.params.id);
-  
-    if (!card) return res.status(404).send('The card with the given ID was not found.');
-  
-    res.send(card);
-  });
-  
-  module.exports = router;
+  let list = await List.findOne({cards: req.params.id});
+  if(list){
+    list.cards = list.cards.filter(x => x != req.params.id);
+    await list.save();
+  }
+  const card = await Card.findByIdAndRemove(req.params.id);
+
+  if (!card) return res.status(404).send('The card with the given ID was not found.');
+
+  res.send(card);
+});
+
+module.exports = router;
--- routes/lists.js
+++ routes/lists.js
@@ -3,112 +3,97 @@ const {List, validate} = require('../models/list');
 const {Card, validateCard = validate} = require('../models/card');
 const express = require('express');
 const _ = require('lodash');
-const Fawn = require('fawn');
 const router = express.Router();
-const mongoose = require('mongoose');
 
-  Fawn.init(mongoose);
 
-  router.get('/', auth, async (req, res) => {
-    const list = await List.find().populate('cards');
-    res.send(list);
-  });
+router.get('/', auth, async (req, res) => {
+  const list = await List.find().populate('cards');
+  res.send(list);
+});
 
-  router.get('/:id', auth, async (req, res) => {
-    const list = await List.findById(req.params.id).populate('cards');
-    res.send(list);
-  });
- 
-  router.post('/', auth,  async (req, res) => {
-    const { error } = validate(req.body); 
-    if (error) return res.status(400).send(error.details[0].message);
-  
-    list = new List(_.pick(req.body, ['title', 'cards']));
-    await list.save();
-  
-    res.send(list);
-  });
+router.get('/:id', auth, async (req, res) => {
+  const list = await List.findById(req.params.id).populate('cards');
+  res.send(list);
+});
 
-  router.post('/:id', auth,  async (req, res) => {
-    const { error } = validateCard(req.body); 
-    if (error) return res.status(400).send(error.details[0].message);
-  
-    let list = await List.findById(req.params.id);
-    if(!list) return res.status(404).send("List not found");
+router.post('/', auth,  async (req, res) => {
+  const { error } = validate(req.body); 
+  if (error) return res.status(400).send(error.details[0].message);
 
-    let card = new Card(_.pick(req.body, ['title', 'description']));
-    card = await card.save();
-    list.cards.push({_id: card._id});
-    list = await list.save();
-    res.send(list);
-  
-    // try {
-    //   new Fawn.Task()
-    //     .save('cards', card)
-    //     .update('lists', { _id: list._id }, { 
-    //       $push: { cards: card }
-    //     })
-    //     .run();
-    
-    //   res.send(list);
-    // }
-    // catch(ex) {
-    //   console.log(ex);
-    //   res.status(500).send('Something failed.');
-    // }
-  });
-  
+  list = new List(_.pick(req.body, ['title', 'cards']));
+  await list.save();
+
+  res.send(list);
+});
+
+router.post('/:id', auth,  async (req, res) => {
+  const { error } = validateCard(req.body); 
+  if (error) return res.status(400).send(error.details[0].message);
+
+  let list = await List.findById(req.params.id);
+  if(!list) return res.status(404).send("List not found");
+
+  let card = new Card(_.pick(req.body, ['title', 'description']));
+  card = await card.save();
+  list.cards.push({_id: card._id});
+  list = await list.save();
+  res.send(list);
+});
+
 router.put('/:id', auth,  async (req, res) => {
-    const { error } = validate(req.body); 
-    if (error) return res.status(400).send(error.details[0].message);
-  
-    const list = await List.findByIdAndUpdate(req.params.id, { title: req.body.title }, {
-      new: true
-    });
-  
-    if (!list) return res.status(404).send('The list with the given ID was not found.');
-    
-    res.send(list);
+  const { error } = validate(req.body); 
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const list = await List.findByIdAndUpdate(req.params.id, { title: req.body.title }, {
+    new: true
   });
 
-  router.put('/:id/:cardId', auth,  async (req, res) => {
-    let listNew = await List.findById(req.params.id);
-    if (!listNew) return res.status(404).send('The list with the given ID was not found.');
-
-    let listOld = await List.findOne({cards: req.params.cardId});
-    listOld.cards = listOld.cards.filter(x => x != req.params.cardId);
-    listNew.cards.push(req.params.cardId);
-
-    listOld.save();
-    listNew.save();
-    
-    res.send(listNew);
-  });
+  if (!list) return res.status(404).send('The list with the given ID was not found.');
   
+  res.send(list);
+});
+
+// Changes card's list
+router.put('/:id/:cardId', auth,  async (req, res) => {
+  let listNew = await List.findById(req.params.id);
+  if (!listNew) return res.status(404).send('The list with the given ID was not found.');
+
+  let listOld = await List.findOne({cards: req.params.cardId});
+  listOld.cards = listOld.cards.filter(x => x != req.params.cardId);
+  listNew.cards.push(req.params.cardId);
+
+  listOld.save();
+  listNew.save();
+  
+  res.send(listNew);
+});
+
+// Removes list with all cards inside
 router.delete('/:id', auth, async (req, res) => {
-    let list = await List.findById(req.params.id);
-    for(x of list.cards) {
-      await Card.findByIdAndRemove(x);
-    }
-    list = await list.remove();
-  
-    if (!list) return res.status(404).send('The list with the given ID was not found.');
-  
-    res.send(list);
-  });
+  let list = await List.findById(req.params.id);
+  for(x of list.cards) {
+    await Card.findByIdAndRemove(x);
+  }
+  list = await list.remove();
 
+  if (!list) return res.status(404).send('The list with the given ID was not found.');
+
+  res.send(list);
+});
+
+// Deletes specified card inside specified list
 router.delete('/:id/:cardId', auth, async (req, res) => {
-    let list = await List.findById(req.params.id);
+  let list = await List.findById(req.params.id);
 
-    list.cards = list.cards.filter(x => x != req.params.cardId);
-    list = list.save();
-    let card = await Card.findByIdAndRemove(req.params.cardId);
+  list.cards = list.cards.filter(x => x != req.params.cardId);
+  list = list.save();
+  let card = await Card.findByIdAndRemove(req.params.cardId);
 
-    if (!list) return res.status(404).send('The list with the given ID was not found.');
-    if (!card) return res.status(404).send('The card with the given ID was not found.');
-  
-    res.send(list, card);
-  });
+  if (!list) return res.status(404).send('The list with the given ID was not found.');
+  if (!card) return res.status(404).send('The card with the given ID was not found.');
+
+  res.send(list, card);
+});
   
 
 module.exports = router;
