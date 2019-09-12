be8fb5e9ce28e4fbf6d920beee2137a1d6c557b1
Wojciech <wojciechboman@gmail.com>
2019-09-12 20:39:53 +0200

    Commit

--- routes/users.js
+++ routes/users.js
@@ -9,7 +9,7 @@ const _ = require('lodash');
 const router = express.Router();
 
 
-outer.get('/me', auth, async (req, res) => {
+router.get('/me', auth, async (req, res) => {
   const user = await User.findById(req.user._id).select('-password');
   res.send(user);
 });
