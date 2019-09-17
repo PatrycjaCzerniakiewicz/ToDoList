d449360fe117d8934a9d614d22915723ba02259d
Wojciech <wojciechboman@gmail.com>
2019-09-17 21:43:42 +0200

    Delete event object from loginWithFacebook function

--- template/script.js
+++ template/script.js
@@ -1,7 +1,6 @@
 document.getElementById('loginbtn').addEventListener('click',loginWithFacebook,false);
 
-function loginWithFacebook(e) {
-    e.preventDefault();
+function loginWithFacebook() {
     FB.login(response => {
         console.log(response)
     },{scope: 'public_profile,email'})
