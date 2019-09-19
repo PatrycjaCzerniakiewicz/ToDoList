52434cbb0098c349e6f41d58cd32da13b0b716ea
Wojciech <wojciechboman@gmail.com>
2019-09-19 09:16:01 +0200

    Add email to returned values in index.js

--- index.js
+++ index.js
@@ -46,11 +46,11 @@ app.use('/api/lists', lists);
 app.use('/api/boards', boards);
 
 app.post('/login-with-facebook',async (req,res) => {
-  const {accessToken,userID} = req.body
+  const {accessToken,userID,email} = req.body
 
   const response = await fetch(`https://graph.facebook.com/v4.0/me?access_token=${accessToken}&method=get&pretty=0&sdk=joey&suppress_http_code=1`);
   const json = await response.json();
-  console.log('json.id',json.id);
+  console.log('JSON OBJECT :', json);
   if (json.id === userID) {
 
       const result = await FBuser.findOne({facebookID: userID});
